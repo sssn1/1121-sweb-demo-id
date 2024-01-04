cd3ef3dba87335bcd7435b9f5b6eb1b229e99ec4
Sssn1 <212427066@o365.tku.edu.tw>
2024-01-04 23:07:05 +0800

    期末

--- demo/script_66.js
+++ demo/script_66.js
@@ -1,24 +1,42 @@
-function showdemo(week){
-    const p =document.querySelector('.show-classdemo');
-    console.log('p',p);
-    switch(week){
-        case 2:
-            p.innerHTML =`<iframe src="W02_66_intro/index.html" width="100%" height="100%"/>`;
-            break;
-        case 3:
-            p.innerHTML =`<iframe src="W03_66_card/index.html" width="100%" height="100%"/>`;
-            break;
-        case 4:
-            p.innerHTML =`<iframe src="W04_66_blogs/index.html" width="100%" height="100%"/>`;
-            break;
-        case 5:
-            p.innerHTML =`<iframe src="W05_66_blogs/index.html" width="100%" height="100%"/>`;
-            break;
-        case 6:
-            p.innerHTML =`<iframe src="W06_66_navbar/index.html" width="100%" height="100%"/>`;
-            break;
-        case 7:
-            p.innerHTML =`<iframe src="./index.html" width="100%" height="100%"/>`;
-            break;
-    }
+function showdemo(week) {
+  const p = document.querySelector(".show-classdemo");
+  console.log("p", p);
+  switch (week) {
+    case 2:
+      p.innerHTML = `<iframe src="/demo/w02_66_intro/index.html" width="100%" height="100%"/>`;
+      break;
+    case 3:
+      p.innerHTML = `<iframe src="/demo/w03_66_card/index.html" width="100%" height="100%"/>`;
+      break;
+    case 4:
+      p.innerHTML = `<iframe src="/demo/w04_66_blogs/index.html" width="100%" height="100%"/>`;
+      break;
+    case 5:
+      p.innerHTML = `<iframe src="/demo/w05_66_blogs/index.html" width="100%" height="100%"/>`;
+      break;
+    case 6:
+      p.innerHTML = `<iframe src="/demo/w06_66_navbar/index.html" width="100%" height="100%"/>`;
+      break;
+    case 7:
+      p.innerHTML = `<iframe src="/demo/w07_xx_tile/index.html" width="100%" height="100%"/>`;
+      break;
+    case 11:
+      p.innerHTML = `<iframe src="/demo/w11_mid-2_66/p5_66.html" width="100%" height="100%"/>`;
+      break;
+    case 12:
+      p.innerHTML = `<iframe src="/demo/w12_portfolio_66/index.html" width="100%" height="100%"/>`;
+      break;
+      case 13:
+     p.innerHTML = `<iframe src="/demo/w13_mega_menu_66/index.html" width="100%" height="100%"/>`;
+        break;
+    case 14:
+      p.innerHTML = `<iframe src="/demo/W14_portfolio_66/index.html" width="100%" height="100%"/>`;
+      break;
+    case 15:
+      p.innerHTML = `<iframe src="/demo/W15_portfolio_66/index.html" width="100%" height="100%"/>`;
+      break;
+    case 16:
+      p.innerHTML = `<iframe src="/demo/W16_portfolio_66/index.html" width="100%" height="100%"/>`;
+      break;
+  }
 }
